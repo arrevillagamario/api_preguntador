9ed6e55e6b6a42063ef6f970d95722115c7535fd
arrevillagamario <marrevillaga03@gmail.com>
2024-05-30 01:03:25 -0600

    bd

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://marrevillaga03:geYOx7RijS144daB@cluster0.oridwoj.mongodb.net/preguntas"
+      "mongodb+srv://marrevillaga03:geYOx7RijS144daB@cluster0.oridwoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Si se piudo ");
   } catch (error) {
